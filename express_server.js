78e54c8349366e51234dfa60426a2e21481e8751
trijaychan <your@email.com>
2022-02-27 06:08:30 +0000

    added /search_results route and body-parser middleware

--- express_server.js
+++ express_server.js
@@ -1,20 +1,26 @@
 // express.js
 
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const PORT = 8080;
 const app = express();
 
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/", (req, res) => {
-  res.render("search_results");
+  res.redirect("/search_results");
 });
 
+app.get("/search_results", (req, res) => {
+  const artistName = req.params.search;
+  res.render("search_results", {artistName});
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
