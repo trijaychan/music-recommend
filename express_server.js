c59060166449eac9762fdbae5c092c127cde9838
trijaychan <your@email.com>
2022-02-27 04:49:41 +0000

    added post route for /

--- express_server.js
+++ express_server.js
@@ -5,6 +5,17 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 
+app.set("view engine", "ejs"); 
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  res.render("search_results");
+});
+
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
