2caa44d177d64fa544efb1efd82aabc3e9dc3f29
trijaychan <your@email.com>
2022-02-28 00:14:04 +0000

    created a function to retrieve an artist's picture

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 // helper.js
 
 const request = require("request-promise");
-const spotifyAccessToken = "BQCURvYXoVl0_lMfOyHH_TtLOFnIZ-OSmHi6z0uzTTfzvBtCpdKlmiBD1uT5L9_4babbcoB8n-8iuPt9ZtZ03FU_YdH4_w44TOPIiofJmB3BVv2dOU8umgiiZ-3WKeP7LaRLZ0UMUlilo9o0AsAxoCDYNt0uaU3BTqk";
+const spotifyAccessToken = "BQDjcUEsnBnj0cNYumGzkK4YWtp4lEOni8RdaRCdk2N6fy3IFF9Cbt3NaZMEf6vEAwTDVqe7gttBMvXaJCm0MQF_fb-M4zNXX6qqeOq6sisGIol7YG7XcP1fDMwhvS0ODYgBHuffI8qQEAt516GDowQ_bCuCJ1JmPb4";
 const headers = { Authorization: `Bearer ${spotifyAccessToken}` };
 
 const fetchArtistSpotify = (artistName) => {
@@ -65,6 +65,20 @@ const recommendedBySpotify = (artistName) => {
     })
 };
 
+const fetchArtistImage = (artistName) => {
+  return fetchArtistSpotify(artistName)
+  .then((body) => {
+    const artistID = JSON.parse(body).artists.items[0].id;
+    const url = `https://api.spotify.com/v1/artists/${artistID}`;
+
+    return request({ url, headers })
+      .then((artistBody) => {
+        return JSON.parse(artistBody).images[0].url;
+      });
+  });
+};
+
 module.exports = {
-  recommendedBySpotify
+  recommendedBySpotify,
+  fetchArtistImage
 };
